1075d2b377c8ac25cfde2b5aa53319846c10e8cc
Hiro Nonoyama <Hiro@Hiro-no-MacBook-Pro-2.local>
2018-03-06 19:12:44 +0900

    React entry point setting

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => {
+  return <div>Hello world</div>;
+};
+
+render(<App />, document.getElementById("react-entry"));
