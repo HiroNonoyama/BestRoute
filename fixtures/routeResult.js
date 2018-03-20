d26da8595fb836ba0d0a3b2ad7699af30be9e849
Hiro Nonoyama <Hiro@Hiro-no-MacBook-Pro-2.local>
2018-03-20 22:46:16 +0900

    wip

---
+++ fixtures/routeResult.js
@@ -0,0 +1,63 @@
+const a = [
+  {
+    "duration": {
+      "text": "18分",
+      "value": 1063
+    },
+    "distance": {
+      "text": "4.0 km",
+      "value": 3990
+    },
+    "end": "日本、東京都新宿区",
+    "start": "日本、東京都東京"
+  },
+  {
+    "duration": {
+      "text": "18時間22分",
+      "value": 66131
+    },
+    "distance": {
+      "text": "1,269 km",
+      "value": 1269014
+    },
+    "end": "日本、北海道",
+    "start": "日本、東京都新宿区"
+  },
+  {
+    "duration": {
+      "text": "17時間58分",
+      "value": 64704
+    },
+    "distance": {
+      "text": "1,245 km",
+      "value": 1244715
+    },
+    "end": "日本、群馬県",
+    "start": "日本、北海道"
+  },
+  {
+    "duration": {
+      "text": "1日 18時間",
+      "value": 149596
+    },
+    "distance": {
+      "text": "2,132 km",
+      "value": 2132460
+    },
+    "end": "日本、沖縄県",
+    "start": "日本、群馬県"
+  },
+  {
+    "duration": {
+      "text": "1日 17時間",
+      "value": 146246
+    },
+    "distance": {
+      "text": "2,115 km",
+      "value": 2114788
+    },
+    "end": "日本、神奈川県横浜市",
+    "start": "日本、沖縄県"
+  }
+];
+module.exports = a
